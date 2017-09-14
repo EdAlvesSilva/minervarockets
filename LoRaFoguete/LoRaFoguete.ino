fabd544440a5358d30d48f1b7f17a79f945f2c02
EdAlvesSilva <EdAlvesSilva@users.noreply.github.com>
2017-09-14 13:20:03 -0300

    Minimal update
    Substituição de uma alocação de variavel que permitiu ocupar menos espaço

--- LoRaFoguete/LoRaFoguete.ino
+++ LoRaFoguete/LoRaFoguete.ino
@@ -28,23 +28,18 @@
 #define RFM95_RST 2
 #define RFM95_INT 3
 #define EEPROMADDRESS 0
+
 // Frequência do LoRa! [COUBRUF 2017: USAR 915.0]
 #define RF95_FREQ 915.0
 
 // Criacao do objeto da classe RH_RF95
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-long packetnum = 0;  // contador do numero de pacotes enviados
 
 void setup() 
 {
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-
-  //Verifica o numero do pacote que sera enviado 
-  //Assim, eh possivel garantir que o numero do pacote
-  //enviado sera sempre o numero certo
-  packetnum = EEPROMReadlong(EEPROMADDRESS);
 
   delay(100);
   
@@ -66,34 +61,39 @@ void setup()
 
 void loop()
 {  
+    //Verifica o numero do pacote que sera enviado 
+  //Assim, eh possivel garantir que o numero do pacote
+  //enviado sera sempre o numero certo
+  long packetnum = EEPROMReadlong();
+   
   unsigned char radiopacket[20] = "ROCKETS!!! #      ";
   itoa(packetnum++, radiopacket+13, 10);
   radiopacket[19] = 0;
   
   rf95.send((uint8_t *)radiopacket, 20);
 
-
-  rf95.waitPacketSent();
   // Espera o pacote ser enviando
+  rf95.waitPacketSent();
+
   
   //Escreve na EEPROM o ultimo pacote enviado
-  EEPROMWritelong(EEPROMADDRESS,(packetnum));
+  EEPROMWritelong((packetnum));
   delay(100);
 }
 
-void EEPROMWritelong(int address, long value)
+void EEPROMWritelong(long value)
       {
 
       //Escreve os 4 bytes no endereco de referencia + os 3 consecutivos
-      EEPROM.write(address, value & 0xFF);//four
-      EEPROM.write(address + 1, (value >> 8) & 0xFF); //three
-      EEPROM.write(address + 2, (value >> 16) & 0xFF); //two
-      EEPROM.write(address + 3, (value >> 24) & 0xFF); //one
+      EEPROM.write(EEPROMADDRESS, value & 0xFF);//four
+      EEPROM.write(EEPROMADDRESS + 1, (value >> 8) & 0xFF); //three
+      EEPROM.write(EEPROMADDRESS + 2, (value >> 16) & 0xFF); //two
+      EEPROM.write(EEPROMADDRESS + 3, (value >> 24) & 0xFF); //one
       }
 
-long EEPROMReadlong(long address)
+long EEPROMReadlong()
       {
       //Le o endereco + os 3 consecutivos
-      return ((EEPROM.read(address) << 0) & 0xFF) + (EEPROM.read(address + 1 << 8) & 0xFFFF) + ((long(EEPROM.read(address + 2)) << 16) & 0xFFFFFF) + ((long(EEPROM.read(address + 3)) << 24) & 0xFFFFFFFF);
+      return ((EEPROM.read(EEPROMADDRESS) << 0) & 0xFF) + (EEPROM.read(EEPROMADDRESS + 1 << 8) & 0xFFFF) + ((long(EEPROM.read(EEPROMADDRESS + 2)) << 16) & 0xFFFFFF) + ((long(EEPROM.read(EEPROMADDRESS + 3)) << 24) & 0xFFFFFFFF);
       }
 
